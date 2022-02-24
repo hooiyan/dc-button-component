a6d29d31fd638f2e0744dfb3e461d97553e48b50
hooiyan <hoecoding@gmail.com>
2022-02-24 10:23:58 +0800

    Add more button variants

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
+import styles from "./styles/Button.module.css";
 
-import Button from './components/Button';
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-      <Button css="default" description="<Button />" />
-      <Button grayText css="defaultHover" description="&:hover, &:focus" />
-      <Button css="outline" description={`<Button variant="outline" />`} />
-      <Button grayText css="outlineHover" description={`&:hover, &:focus`} />
-      <Button css="text" description={`<Button variant="text" />`} />
-      <Button css="textHover" description="&:hover, &:focus" />
+      <section className={styles.group}>
+        <Button css="default" description="<Button />" />
+        <Button grayText css="defaultHover" description="&:hover, &:focus" />
+      </section>
+      <section className={styles.group}>
+        <Button css="outline" description={`<Button variant="outline" />`} />
+        <Button grayText css="outlineHover" description={`&:hover, &:focus`} />
+      </section>
+      <section className={styles.group}>
+        <Button css="text" description={`<Button variant="text" />`} />
+        <Button grayText css="textHover" description="&:hover, &:focus" />
+      </section>
+      <Button css="disableShadow" description="<Button disableShadow />" />
+      <section className={styles.group}>
+        <Button
+          css="disabled"
+          description="<Button disabled />"
+          text="Disabled"
+        />
+        <Button
+          css="textDisabled"
+          description={`<Button variant="text" disabled />`}
+          text="Disabled"
+        />
+      </section>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,14 @@
-import styles from '../styles/Button.module.css';
+import styles from "../styles/Button.module.css";
 
 const Button = ({ css, description, grayText, text = "Default" }) => {
   return (
     <div>
       <p className={grayText ? styles.grayName : styles.name}>{description}</p>
-      <button className={[styles[`${css}`], styles.btn].join(' ')}>{text}</button>
+      <button className={[styles[`${css}`], styles.btn].join(" ")}>
+        {text}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Button;
